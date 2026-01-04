dfe0d39cfcf28c60d896cc986cfedce0d7ebc890
Vecortys <123871947+Vecortys@users.noreply.github.com>
2026-01-04 23:57:52 +0300

    revert pipe-pressure-damage

--- Content.Server/_Erida/Atmos/ExplodeByPressure/ExplodeByPressureComponent.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Content.Server._Erida.Atmos.ExplodeByPressure;
-
-[RegisterComponent]
-public sealed partial class ExplodeByPressureComponent : Component
-{
-    [DataField]
-    public float PressureLimit = 15000f;
-}
